b2f651414cca0ed1042bae8534a8ce95a92db058
x8lua <155454885+x8lua@users.noreply.github.com>
2026-05-10 16:51:44 +0800

    Wrap Pages Router with ClerkProvider

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import { ClerkProvider } from '@clerk/nextjs';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
