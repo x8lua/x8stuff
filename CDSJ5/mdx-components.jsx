fded5337a963a8f1b71ea4ebf7a6f9e70bf10c3d
x8lua <155454885+x8lua@users.noreply.github.com>
2026-05-16 00:07:52 +0800

    Use Nextra docs theme MDX components

--- CDSJ5/mdx-components.jsx
+++ CDSJ5/mdx-components.jsx
@@ -1,3 +1,7 @@
-export function useMDXComponents(c) {
-  return c
+import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs'
+
+const themeComponents = getThemeComponents()
+
+export function useMDXComponents(components = {}) {
+  return Object.assign({}, themeComponents, components)
 }
